7acb9d79f277619b6d181f71460f0a2c90874127
Eugene Komarov <eugene.komarov@dif.tech>
2023-06-08 09:30:34 +0200

    atomic ref option

--- app/src/main/java/com/opengamer/sharedupdatertest/SharedCacheUpdater.kt
+++ app/src/main/java/com/opengamer/sharedupdatertest/SharedCacheUpdater.kt
@@ -1,25 +1,35 @@
 package com.opengamer.sharedupdatertest
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.shareIn
+import java.util.concurrent.atomic.AtomicReference
 
 class SharedCacheUpdater<Result : Any>(
-    updateScope: CoroutineScope,
+    private val updateScope: CoroutineScope,
     private val updater: suspend () -> Result,
 ) {
 
-    private val value = flow<Result> {
+    private val sharedUpdater: AtomicReference<Flow<Result>?> = AtomicReference(null)
+
+    suspend fun getShared(): Result = (
+        sharedUpdater.get() ?: sharedUpdater.updateAndGet {
+            if (it != null) return@updateAndGet it
+            createShared()
+        }!!
+        ).first()
+
+    private fun createShared(): Flow<Result> = flow<Result> {
         emit(updater.invoke())
     }.shareIn(
         scope = updateScope,
-        started = SharingStarted.WhileSubscribed(
-            0,
-            0,
-        ),
+        started = SharingStarted.WhileSubscribed(),
     )
-
-    suspend fun getShared(): Result = value.first()
+        .onCompletion {
+            sharedUpdater.set(null)
+        }
 }
