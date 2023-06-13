c4fe34763ec620552c2a52bd0d5ba169311dd088
Eugene Komarov <eugene.komarov@dif.tech>
2023-06-13 19:58:47 +0200

    own shared flow

--- app/src/main/java/com/opengamer/sharedupdatertest/SharedCacheUpdater.kt
+++ app/src/main/java/com/opengamer/sharedupdatertest/SharedCacheUpdater.kt
@@ -1,25 +1,66 @@
 package com.opengamer.sharedupdatertest
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 class SharedCacheUpdater<Result : Any>(
-    updateScope: CoroutineScope,
+    private val updateScope: CoroutineScope,
     private val updater: suspend () -> Result,
 ) {
 
-    private val value = flow<Result> {
-        emit(updater.invoke())
-    }.shareIn(
-        scope = updateScope,
-        started = SharingStarted.WhileSubscribed(
-            0,
-            0,
-        ),
-    )
+    private val sharedMutex = Mutex()
 
-    suspend fun getShared(): Result = value.first()
+    private val shared = MutableSharedFlow<Result>(
+        replay = 0,
+        extraBufferCapacity = 0,
+        onBufferOverflow = BufferOverflow.SUSPEND,
+    ).apply {
+        updateScope.launch {
+            subscriptionCount
+                .collectLatest {
+                    println("subscribers $it")
+                    if (it > 0) {
+                        ensureActiveUpdate()
+                    } else {
+                        updaterJob = null
+                    }
+                }
+        }
+    }
+
+    @Volatile
+    private var updaterJob: Job? = null
+        set(value) {
+            field?.cancel()
+            field = value
+        }
+
+    suspend fun getShared(): Result = shared.first()
+
+    private suspend fun ensureActiveUpdate() {
+        println("ensureActiveUpdate")
+        sharedMutex.withLock {
+            if (updaterJob == null || updaterJob?.isActive == false) {
+                startUpdate()
+            }
+        }
+    }
+
+    private fun startUpdate() {
+        println("startUpdate")
+        updaterJob = updateScope.launch {
+            val newValue = updater.invoke()
+            sharedMutex.withLock {
+                shared.emit(newValue)
+                updaterJob = null
+            }
+        }
+    }
 }
